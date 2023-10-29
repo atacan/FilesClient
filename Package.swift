f56cbcc48cfdf49c9d9bab9c5e11a4598dbc5bfc
atacan <atacandur@icloud.com>
2023-10-29 11:58:12 +0100

    initial

--- Package.swift
+++ Package.swift
@@ -5,17 +5,26 @@ import PackageDescription
 
 let package = Package(
     name: "FilesClient",
+    platforms: [
+            .macOS(.v13),
+        ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "FilesClient",
             targets: ["FilesClient"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "1.0.0"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "FilesClient"),
+            name: "FilesClient",
+            dependencies: [
+                .product(name: "Dependencies", package: "swift-dependencies")
+            ]),
         .testTarget(
             name: "FilesClientTests",
             dependencies: ["FilesClient"]),
--- Sources/FilesClient/FilesClient.swift
+++ Sources/FilesClient/FilesClient.swift
@@ -1,2 +1,40 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Dependencies
+import Foundation
+import XCTestDynamicOverlay
+
+public struct FilesClient {
+    public var read: @Sendable (URL) async throws -> String
+    public var temporaryDirectory: @Sendable () -> URL
+    public var temporaryFileWithExtension: @Sendable (String) -> URL
+
+    // function versions with named arguments of the above
+    public func read(url: URL) async throws -> String {
+        try await read(url)
+    }
+    public func temporaryFile(withExtension: String) -> URL {
+        temporaryFileWithExtension(withExtension)
+    }
+}
+
+extension FilesClient: DependencyKey {
+    public static var liveValue: Self {
+        @Dependency(\.uuid) var uuid
+
+        return Self(
+            read: { try String(contentsOf: $0) },
+            temporaryDirectory: { URL(fileURLWithPath: NSTemporaryDirectory()) },
+            temporaryFileWithExtension: {
+                URL(fileURLWithPath: NSTemporaryDirectory())
+                    .appendingPathComponent(uuid().uuidString)
+                    .appendingPathExtension($0)
+            }
+        )
+    }
+}
+
+extension DependencyValues {
+    public var filesClient: FilesClient {
+        get { self[FilesClient.self] }
+        set { self[FilesClient.self] = newValue }
+    }
+}
